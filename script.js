28fd460e9357edefcd5a4c941d421720be4e806c
Felipe <felipeeduardofreitasm@gmail.com>
2024-10-06 21:30:45 -0300

    Correcao de alguns bugs, responsividade, adicao de detalhes.

--- script.js
+++ script.js
@@ -1,34 +1,36 @@
-function mostrarResultadoTabela(lista) {
-    const tabela = document.getElementById('table');
+function showTableResult(lista) {
+    const table = document.getElementById('table');
     for (let i = 0; i < lista.length; i++) {
         for(let j = 0; j < lista[i].length; j++) {
-            tabela.rows[i + 1].cells[j].innerHTML = lista[i][j];
+            table.rows[i + 1].cells[j].innerHTML = lista[i][j];
+        }
     }
 }
 
-}
-
-document.getElementById('meuBotao').addEventListener('click', function () {
-
-    const numero = parseInt(document.getElementById('numero-input').value);
-
-    const aviso = document.querySelector('.aviso');
-
-    if (isNaN(numero) || numero == "") {
-        aviso.innerText = "Digite um número!";
+document.getElementById('generate-button').addEventListener('click', function () {
+    
+    const number = document.getElementById('number-input').value.trim();
+    const warning = document.querySelector('.warning');
+    if (!/^\d+$/.test(number)) {
+        warning.innerText = "Digite um número inteiro!";
         return;
     }
 
-    let lista_numero = []
-
-    for (i = 1; i <= 10; i++) {
-        let resultado = i * numero;
-        lista_numero.push([i, numero, resultado])
+    const numberLimit = 100000;
+    if (number > numberLimit) {
+        warning.innerText = "Digite um número menor que " + numberLimit;
+        return;
     }
 
-    aviso.innerText = " ";
-    mostrarResultadoTabela(lista_numero);
+    const numeroInt = parseInt(number);
+    let numberList = []
+    for (i = 1; i <= 10; i++) {
+        let result = i * numeroInt;
+        numberList.push([i, numeroInt, result]);
+    }
+
+    warning.innerText = "";
+
+    showTableResult(numberList);
     
 });
-
-
